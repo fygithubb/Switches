b9ef6add88919b71af16d242e941008a44645df9
ANAS KHAN <khananas280@gmail.com>
2025-07-06 03:47:07 +0530

    Added GameController to handle Actions

---
+++ Controllers/GameController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Switches.Services;
+
+namespace Switches.Controllers
+{
+    public class GameController : Controller
+    {
+        private static SwitchGameService _game = new SwitchGameService();
+
+        // GET: /Game
+        public IActionResult Index()
+        {
+            return View(_game);
+        }
+
+        // POST: /Game/Press
+        [HttpPost]
+        public IActionResult Press(int id)
+        {
+            _game.PressSwitch(id);
+            return RedirectToAction("Index");
+        }
+
+        // POST: /Game/Reset
+        [HttpPost]
+        public IActionResult Reset()
+        {
+            _game.Reset();
+            return RedirectToAction("Index");
+        }
+    }
+}
+
+
+
