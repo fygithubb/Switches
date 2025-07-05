52ef25263548a1bdaaedd7d789561048216f7e75
ANAS KHAN <khananas280@gmail.com>
2025-07-06 03:46:45 +0530

    Added GameService logic;

---
+++ Services/SwitchGameService.cs
@@ -0,0 +1,44 @@
+﻿namespace Switches.Services
+{
+    public class SwitchGameService
+    {
+        private readonly int[] toggleMasks = new int[]
+        {
+            0b110,   //Switch A affect A and B
+            0b011,   // Switch B affects B and C
+            0b101    // Switch C affects C
+        };
+
+        public int CurrentState { get; private set; } = 0b000;
+        public int MoveCount { get; private set; } = 0;
+        public int MaxMoves { get; private set; } = 3;
+
+
+        //status properties
+        public bool IsComplete => CurrentState == 0b111;
+        public bool OutOfMoves => MoveCount >= MaxMoves;
+
+        //current should be returned as binary string
+        public string GetStateString()
+        {
+            return Convert.ToString(CurrentState, 2).PadLeft(3, '0');
+        }
+
+
+        public void PressSwitch(int index)
+        {
+            if (IsComplete || OutOfMoves) return;
+            if (index < 0 || index >= toggleMasks.Length ) return;
+
+            CurrentState ^= toggleMasks[index];
+            MoveCount++;
+        }
+
+
+        public void Reset()
+        {
+            CurrentState = 0b000;
+            MoveCount = 0;
+        }
+    }
+}
