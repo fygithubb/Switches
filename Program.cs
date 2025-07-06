7b8efe4aa95c53a0abf2ed4792aeca37d8b92fcd
ANAS KHAN <khananas280@gmail.com>
2025-07-06 16:58:13 +0530

    Session changes

--- Program.cs
+++ Program.cs
@@ -7,16 +7,9 @@ namespace Switches
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
-
             builder.Services.AddDistributedMemoryCache();
-            builder.Services.AddSession(options =>
-            {
-                options.IdleTimeout = TimeSpan.FromMinutes(30);
-                options.Cookie.HttpOnly = true;
-                options.Cookie.IsEssential = true;
-            });
-
+            builder.Services.AddSession();
+            builder.Services.AddControllersWithViews();
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -29,9 +22,8 @@ namespace Switches
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseSession(); 
             app.UseRouting();
-
+            app.UseSession();
             app.UseAuthorization();
 
             app.MapControllerRoute(
